3d7d1209bfe20d4bdaaf43ec5e2caa97a4e15127
tlimakun <t.limakun@gmail.com>
2021-05-25 19:30:36 +0700

    Add for loop to call all callback functions from filter_callbacks.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import dash
 from data_preprocessing import load_data
 from filter_layout import filter_layout
+import filter_callbacks
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
@@ -10,6 +11,13 @@ filename = "../data/sample-data-1-7Mar.xlsx"
 days = load_data(filename)
 
 app.layout = filter_layout(days)
+
+# Call all callback functions from filter_callbacks.py
+for i in dir(filter_callbacks):
+    item = getattr(filter_callbacks, i)
+    
+    if callable(item) and i.startswith("callback_"):
+        item(app, days)
 
 if __name__ == "__main__":
     app.run_server(host="0.0.0.0", debug=True)
