2c7aa8fdd6d03da6586365e949b63afd139220f3
tlimakun <t.limakun@gmail.com>
2021-05-25 19:31:32 +0700

    Add total visitors label and data table

--- filter_components.py
+++ filter_components.py
@@ -4,6 +4,10 @@ import dash_table as table
 from datetime import timedelta
 
 def generate_date_picker_range(days):
+    """
+    Generate date picker using DatePickerRange from Dash Core Components.
+    """
+    
     dates = list(days.keys())
     
     return dcc.DatePickerRange(
@@ -15,4 +19,44 @@ def generate_date_picker_range(days):
         minimum_nights=0,
         initial_visible_month=min(dates),
         display_format="D/M/YYYY"
+    )
+    
+def generate_total_visitors_label():
+    """
+    Generate total visitors label using Label from Dash Html Components.
+    """
+    
+    return html.Label(
+        id="total-visitors-label",
+        style={
+            "marginTop": 5,
+            "marginBottom": 5
+        }
+    )
+    
+def generate_data_table(days):
+    """
+    Generate data table using Dash DataTable
+    """
+    
+    columns = days.get(list(days.keys())[0]).columns
+    
+    return table.DataTable(
+        id="data-table",
+        columns=[{"name": col, "id": col} for col in columns],
+        page_size=5,
+        style_cell={
+            "whiteSpace": "normal",
+            "height": "auto",
+            "lineHeight": "20px",
+            "minLineHeight": "20px",
+            "textAlign": "left"
+        },
+        style_cell_conditional=[
+            {"if": {"column_id": dt_column},
+             "width": "6%"} for dt_column in [col for col in columns if col.endswith("_dt")]
+        ] + [{"if": {"column_id": "clinic"},
+              "width": "10%"},
+             {"if": {"column_id": "sex"},
+              "width": "2%"}]
     )
