309cfd99485c50af4efb356f2e2d6e30742b47c3
tlimakun <t.limakun@gmail.com>
2021-05-25 18:51:19 +0700

    Add Layout

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import dash
 from data_preprocessing import load_data
+from filter_layout import filter_layout
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
@@ -7,6 +8,8 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
 filename = "../data/sample-data-1-7Mar.xlsx"
 days = load_data(filename)
+
+app.layout = filter_layout(days)
 
 if __name__ == "__main__":
     app.run_server(host="0.0.0.0", debug=True)
