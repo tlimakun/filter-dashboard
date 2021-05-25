f30636d3f31d459a922180040e48cb48ae2f5919
tlimakun <t.limakun@gmail.com>
2021-05-25 19:31:02 +0700

    Add total visitors label and data table

--- filter_layout.py
+++ filter_layout.py
@@ -2,11 +2,26 @@ import dash_html_components as html
 from filter_components import *
 
 def filter_layout(days):
+    """
+    Main layout of filter dashboard.
+    """
+    
     return html.Div([
+        # Date picker range division
         html.Div([
-            html.Label(children="Date : ",
-                       style={"display": "inline-block",
-                              "marginRight": 10}),
+            html.Label(
+                children="Date : ",
+                style={"display": "inline-block",
+                       "marginRight": 10}),
             generate_date_picker_range(days)
-        ], style={"marginBottom": 5})
+        ], style={"marginBottom": 5}),
+        
+        # Data table division
+        html.Div([
+            # Total visitors label
+            generate_total_visitors_label(),
+            
+            # Data table
+            generate_data_table(days)
+        ])
     ])
