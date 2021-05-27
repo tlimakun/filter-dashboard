737277db12ef2709ba1e7d5b2b1eaa2ede4f20d4
tlimakun <t.limakun@gmail.com>
2021-05-27 16:45:22 +0700

    Edit data table callback

--- filter_callbacks.py
+++ filter_callbacks.py
@@ -21,6 +21,93 @@ def filter_data_by_date(days, start_date, end_date):
     
     return filtered
 
+def datetime_columns_dict(kios_g_dt, kios_dt, screen_dt, send_doc_dt, doc_call_dt, doc_begin_dt,
+                          doc_submit_dt, nurse_dt, payment_dt, pharmacy_dt):
+    datetime_dict = {"kios_g_dt": kios_g_dt, "kios_dt": kios_dt, "screen_dt": screen_dt, "send_doc_dt": send_doc_dt,
+                     "doc_call_dt": doc_call_dt, "doc_begin_dt": doc_begin_dt, "doc_submit_dt": doc_submit_dt,
+                     "nurse_dt": nurse_dt, "payment_dt": payment_dt, "pharmacy_dt": pharmacy_dt}
+    
+    return datetime_dict
+
+def callback_data_table(app, days):
+    """
+    Update data in "data-table" table and number of visitors in "total-visitors-label" label.
+    """
+    
+    @app.callback(
+        Output("total-visitors-label", "children"),
+        Output("data-table", "data"),
+        Input("date-picker-range", "start_date"),
+        Input("date-picker-range", "end_date"),
+        Input("gender-checklist", "value"),
+        Input("final-status-checklist", "value"),
+        Input("appointment-checklist", "value"),
+        Input("min-age-input", "value"),
+        Input("max-age-input", "value"),
+        Input("min-start-time-input", "value"),
+        Input("max-start-time-input", "value"),
+        Input("min-total-time-input", "value"),
+        Input("max-total-time-input", "value"),
+        Input("clinics-checklist", "value"),
+        Input("kios-g-column-radioItems", "value"),
+        Input("kios-column-radioItems", "value"),
+        Input("screen-column-radioItems", "value"),
+        Input("send-doc-column-radioItems", "value"),
+        Input("doc-call-column-radioItems", "value"),
+        Input("doc-begin-column-radioItems", "value"),
+        Input("doc-submit-column-radioItems", "value"),
+        Input("nurse-column-radioItems", "value"),
+        Input("payment-column-radioItems", "value"),
+        Input("pharmacy-column-radioItems", "value")
+    )
+    def update_data_table(start_date, end_date, gender, final_status, appointment, min_age, max_age,
+                          min_start_time, max_start_time, min_total_time, max_total_time, clinics,
+                          kios_g_dt, kios_dt, screen_dt, send_doc_dt, doc_call_dt, doc_begin_dt,
+                          doc_submit_dt, nurse_dt, payment_dt, pharmacy_dt):
+        # Filter data between given start date and end date.
+        filtered = filter_data_by_date(days, start_date, end_date)
+        
+        # Filter appointment
+        if len(appointment) == 1:
+            if 1 in appointment:
+                filtered = filtered[(filtered["visit_dt"].dt.minute.isin([0, 30])) & (filtered["visit_dt"].dt.second == 0)]
+            elif 0 in appointment:
+                filtered = filtered[(~filtered["visit_dt"].dt.minute.isin([0, 30])) | (filtered["visit_dt"].dt.second != 0)]
+        elif len(appointment) == 0:
+            filtered = filtered[filtered["vn"] == -1]
+            
+        # Filter datetime columns
+        for col, value in datetime_columns_dict(kios_g_dt, kios_dt, screen_dt, send_doc_dt, doc_call_dt, doc_begin_dt,
+                                                doc_submit_dt, nurse_dt, payment_dt, pharmacy_dt).items():
+            if value == 1:
+                filtered = filtered[~filtered[col].isna()]
+            elif value == 0:
+                filtered = filtered[filtered[col].isna()]
+        
+        # Filter gender, final status, age, and clinics
+        filtered = filtered[(filtered["gender"].isin(gender)) &
+                            (filtered["final_status"].isin(final_status)) &
+                            (filtered["age"] >= min_age) & (filtered["age"] <= max_age) &
+                            (filtered["clinic"].isin(clinics))]
+        
+        # Filter visitors with start time between min_start_time and max_start_time
+        filtered["start_time"] = filtered[[col for col in filtered.columns if col.endswith("_dt") and col != "visit_dt"]].min(axis=1)
+        filtered = filtered[(filtered["start_time"].dt.hour >= min_start_time) & (filtered["start_time"].dt.hour < max_start_time)]
+        
+        # Filter visitors with total time in process between min_total_time and max_total_time
+        filtered["end_time"] = filtered[[col for col in filtered.columns if col.endswith("_dt") and col != "visit_dt"]].max(axis=1)
+        filtered["total_time"] = (filtered["end_time"] - filtered["start_time"]) / np.timedelta64(1, 'h')
+        filtered = filtered[(filtered["total_time"] >= min_total_time) & (filtered["total_time"] < max_total_time )]
+        
+        # Drop unused columns
+        filtered.drop(["start_time", "end_time", "total_time"], axis=1)
+        
+        # Change format in datetime columns.
+        for col in [col for col in filtered.columns if col.endswith("_dt")]:
+            filtered[col] = filtered[col].dt.strftime("%d-%m-%Y%n%H:%M:%S")
+        
+        return f"Total filtered visitors: {len(filtered)}", filtered.to_dict("records")
+
 def callback_age_inputs(app, days):
     """
     Update max, min and initial values of age inputs.
@@ -69,62 +156,3 @@ def callback_clinics_checklist(app, days):
         options = sorted(options, key=(lambda key: key["disabled"]))
         
         return options, available_clinics
-
-def callback_data_table(app, days):
-    """
-    Update data in "data-table" table and number of visitors in "total-visitors-label" label.
-    """
-    
-    @app.callback(
-        Output("total-visitors-label", "children"),
-        Output("data-table", "data"),
-        Input("date-picker-range", "start_date"),
-        Input("date-picker-range", "end_date"),
-        Input("gender-checklist", "value"),
-        Input("final-status-checklist", "value"),
-        Input("appointment-checklist", "value"),
-        Input("min-age-input", "value"),
-        Input("max-age-input", "value"),
-        Input("min-start-time-input", "value"),
-        Input("max-start-time-input", "value"),
-        Input("min-total-time-input", "value"),
-        Input("max-total-time-input", "value"),
-        Input("clinics-checklist", "value")
-    )
-    def update_data_table(start_date, end_date, gender, final_status, appointment, min_age, max_age,
-                          min_start_time, max_start_time, min_total_time, max_total_time, clinics):
-        # Filter data between given start date and end date.
-        filtered = filter_data_by_date(days, start_date, end_date)
-        
-        # Filter appointment
-        if len(appointment) == 1:
-            if 1 in appointment:
-                filtered = filtered[(filtered["visit_dt"].dt.minute.isin([0, 30])) & (filtered["visit_dt"].dt.second == 0)]
-            elif 0 in appointment:
-                filtered = filtered[(~filtered["visit_dt"].dt.minute.isin([0, 30])) | (filtered["visit_dt"].dt.second != 0)]
-        elif len(appointment) == 0:
-            filtered = filtered[filtered["vn"] == -1]
-        
-        # Filter gender, final status, age, and clinics
-        filtered = filtered[(filtered["gender"].isin(gender)) &
-                            (filtered["final_status"].isin(final_status)) &
-                            (filtered["age"] >= min_age) & (filtered["age"] <= max_age) &
-                            (filtered["clinic"].isin(clinics))]
-        
-        # Filter visitors with start time between min_start_time and max_start_time
-        filtered["start_time"] = filtered[[col for col in filtered.columns if col.endswith("_dt") and col != "visit_dt"]].min(axis=1)
-        filtered = filtered[(filtered["start_time"].dt.hour >= min_start_time) & (filtered["start_time"].dt.hour < max_start_time)]
-        
-        # Filter visitors with total time in process between min_total_time and max_total_time
-        filtered["end_time"] = filtered[[col for col in filtered.columns if col.endswith("_dt") and col != "visit_dt"]].max(axis=1)
-        filtered["total_time"] = (filtered["end_time"] - filtered["start_time"]) / np.timedelta64(1, 'h')
-        filtered = filtered[(filtered["total_time"] >= min_total_time) & (filtered["total_time"] < max_total_time )]
-        
-        # Drop unused columns
-        filtered.drop(["start_time", "end_time", "total_time"], axis=1)
-        
-        # Change format in datetime columns.
-        for col in [col for col in filtered.columns if col.endswith("_dt")]:
-            filtered[col] = filtered[col].dt.strftime("%d-%m-%Y%n%H:%M:%S")
-        
-        return f"Total filtered visitors: {len(filtered)}", filtered.to_dict("records")
