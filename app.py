929cc8ac559abd9b89fb4276efa019cbadc0820b
tlimakun <t.limakun@gmail.com>
2021-05-28 16:38:11 +0700

    Add config

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ import filter_callbacks
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(__name__,
-                external_stylesheets=external_stylesheets)
+                external_stylesheets=external_stylesheets,
+                suppress_callback_exceptions=True)
 
 filename = "../data/sample-data-1-7Mar.xlsx"
 days = load_data(filename)
